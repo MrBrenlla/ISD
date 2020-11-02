eaad54f2c67812dc33c3f7c313795bfcc1654ab1
Carlos Fernández Armán <carlos.fernandez.arman@udc.es>
2020-11-02 18:57:09 +0100

    Clases básicas y UML (faltan relacciones)

--- ws-app-model/src/main/java/es/udc/isd060/runfic/model/carrera/Carrera.java
+++ ws-app-model/src/main/java/es/udc/isd060/runfic/model/carrera/Carrera.java
@@ -1,4 +1,125 @@
 package es.udc.isd060.runfic.model.carrera;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Carrera {
+    private int idCarrera;
+    private String ciudadCelebracion;
+    private String descripcion;
+    private Float precioInscripcion;
+    private LocalDateTime fechaAlta;
+    private LocalDateTime fechaCelebracion;
+    private int plazasDisponibles;
+    private int plazasOcupadas;
+
+
+    public Carrera(int idCarrera, String ciudadCelebracion, String descripcion, Float precioInscripcion, LocalDateTime fechaAlta, LocalDateTime fechaCelebracion, int plazasDisponibles, int plazasOcupadas) {
+        this.idCarrera = idCarrera;
+        this.ciudadCelebracion = ciudadCelebracion;
+        this.descripcion = descripcion;
+        this.precioInscripcion = precioInscripcion;
+        this.fechaAlta = fechaAlta;
+        this.fechaCelebracion = fechaCelebracion;
+        this.plazasDisponibles = plazasDisponibles;
+        this.plazasOcupadas = plazasOcupadas;
+    }
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+
+    public String getCiudadCelebracion() {
+        return ciudadCelebracion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Float getPrecioInscripcion() {
+        return precioInscripcion;
+    }
+
+    public LocalDateTime getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public LocalDateTime getFechaCelebracion() {
+        return fechaCelebracion;
+    }
+
+    public int getPlazasDisponibles() {
+        return plazasDisponibles;
+    }
+
+    public int getPlazasOcupadas() {
+        return plazasOcupadas;
+    }
+
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public void setCiudadCelebracion(String ciudadCelebracion) {
+        this.ciudadCelebracion = ciudadCelebracion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecioInscripcion(Float precioInscripcion) {
+        this.precioInscripcion = precioInscripcion;
+    }
+
+    public void setFechaAlta(LocalDateTime fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public void setFechaCelebracion(LocalDateTime fechaCelebracion) {
+        this.fechaCelebracion = fechaCelebracion;
+    }
+
+    public void setPlazasDisponibles(int plazasDisponibles) {
+        this.plazasDisponibles = plazasDisponibles;
+    }
+
+    public void setPlazasOcupadas(int plazasOcupadas) {
+        this.plazasOcupadas = plazasOcupadas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Carrera)) return false;
+        Carrera carrera = (Carrera) o;
+        return getIdCarrera() == carrera.getIdCarrera() &&
+                getPlazasDisponibles() == carrera.getPlazasDisponibles() &&
+                getPlazasOcupadas() == carrera.getPlazasOcupadas() &&
+                Objects.equals(getCiudadCelebracion(), carrera.getCiudadCelebracion()) &&
+                Objects.equals(getDescripcion(), carrera.getDescripcion()) &&
+                Objects.equals(getPrecioInscripcion(), carrera.getPrecioInscripcion()) &&
+                Objects.equals(getFechaAlta(), carrera.getFechaAlta()) &&
+                Objects.equals(getFechaCelebracion(), carrera.getFechaCelebracion());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIdCarrera(), getCiudadCelebracion(), getDescripcion(), getPrecioInscripcion(), getFechaAlta(), getFechaCelebracion(), getPlazasDisponibles(), getPlazasOcupadas());
+    }
+
+    @Override
+    public String toString() {
+        return "Carrera{" +
+                "idCarrera=" + idCarrera +
+                ", ciudadCelebracion='" + ciudadCelebracion + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precioInscripcion=" + precioInscripcion +
+                ", fechaAlta=" + fechaAlta +
+                ", fechaCelebracion=" + fechaCelebracion +
+                ", plazasDisponibles=" + plazasDisponibles +
+                ", plazasOcupadas=" + plazasOcupadas +
+                '}';
+    }
 }
--- ws-app-model/src/main/java/es/udc/isd060/runfic/model/inscripcion/Inscripcion.java
+++ ws-app-model/src/main/java/es/udc/isd060/runfic/model/inscripcion/Inscripcion.java
@@ -1,4 +1,113 @@
 package es.udc.isd060.runfic.model.inscripcion;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Inscripcion {
+    private int idInscripcion;
+    private int idCarrera;
+    private int dorsal;
+    private String numTarjeta;
+    private String emailUsuario;
+    private LocalDateTime fechaInscripcion;
+    private boolean dorsalEsRecogido;
+
+    public Inscripcion(int idInscripcion, int idCarrera, int dorsal, String numTarjeta, String emailUsuario, LocalDateTime fechaInscripcion, boolean dorsalEsRecogido) {
+        this.idInscripcion = idInscripcion;
+        this.idCarrera = idCarrera;
+        this.dorsal = dorsal;
+        this.numTarjeta = numTarjeta;
+        this.emailUsuario = emailUsuario;
+        this.fechaInscripcion = fechaInscripcion;
+        this.dorsalEsRecogido = dorsalEsRecogido;
+    }
+
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public String getNumTarjeta() {
+        return numTarjeta;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public LocalDateTime getFechaInscripcion() {
+        return fechaInscripcion;
+    }
+
+    public boolean isDorsalEsRecogido() {
+        return dorsalEsRecogido;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public void setNumTarjeta(String numTarjeta) {
+        this.numTarjeta = numTarjeta;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public void setFechaInscripcion(LocalDateTime fechaInscripcion) {
+        this.fechaInscripcion = fechaInscripcion;
+    }
+
+    public void setDorsalEsRecogido(boolean dorsalEsRecogido) {
+        this.dorsalEsRecogido = dorsalEsRecogido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Inscripcion)) return false;
+        Inscripcion that = (Inscripcion) o;
+        return getIdInscripcion() == that.getIdInscripcion() &&
+                getIdCarrera() == that.getIdCarrera() &&
+                getDorsal() == that.getDorsal() &&
+                isDorsalEsRecogido() == that.isDorsalEsRecogido() &&
+                Objects.equals(getNumTarjeta(), that.getNumTarjeta()) &&
+                Objects.equals(getEmailUsuario(), that.getEmailUsuario()) &&
+                Objects.equals(getFechaInscripcion(), that.getFechaInscripcion());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIdInscripcion(), getIdCarrera(), getDorsal(), getNumTarjeta(), getEmailUsuario(), getFechaInscripcion(), isDorsalEsRecogido());
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" +
+                "idInscripcion=" + idInscripcion +
+                ", idCarrera=" + idCarrera +
+                ", dorsal=" + dorsal +
+                ", numTarjeta='" + numTarjeta + '\'' +
+                ", emailUsuario='" + emailUsuario + '\'' +
+                ", fechaInscripcion=" + fechaInscripcion +
+                ", dorsalEsRecogido=" + dorsalEsRecogido +
+                '}';
+    }
 }
