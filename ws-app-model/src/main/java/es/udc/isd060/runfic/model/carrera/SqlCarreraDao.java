cac3a2f42846418dadf5591ff255b8be8c7db7da
Carlos Fernández Armán <carlos.fernandez.arman@udc.es>
2020-11-02 20:33:37 +0100

    UML Completado , Cabeceras Daos y asignacion funciones

---
+++ ws-app-model/src/main/java/es/udc/isd060/runfic/model/carrera/SqlCarreraDao.java
@@ -0,0 +1,26 @@
+package es.udc.isd060.runfic.model.carrera;
+
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface SqlCarreraDao {
+
+    // Autor : Yago
+    public Carrera create ( Connection connection , Carrera carrera);
+
+    // Autor : Carlos
+    public Carrera find ( Connection connection , int idCarrera) throws InstanceNotFoundException;
+
+    // Autor : Yago
+    public List<Carrera> find (Connection connection , LocalDateTime fechaCelebracion , String ciudad );// InvalidDateException
+
+    // Autor : Brais
+    public void update ( Connection connection , Carrera carrera ) throws InstanceNotFoundException;
+
+    // Autor : Yago
+    public void remove ( Connection connection , Carrera carrera ) throws InstanceNotFoundException;
+
+
+}
---
+++ ws-app-model/src/main/java/es/udc/isd060/runfic/model/inscripcion/SqlInscripcionDao.java
@@ -0,0 +1,25 @@
+package es.udc.isd060.runfic.model.inscripcion;
+
+import com.sun.jdi.connect.spi.Connection;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+
+import java.util.List;
+
+public interface SqlInscripcionDao {
+
+    // Autor : Brais
+    public Inscripcion create(Connection connection, Inscripcion inscripcion);// fullCapacityException , inscriptionsAlreadyClosed
+
+    // Autor : Carlos
+    public Inscripcion find(Connection connection, int idInscripcion) throws InstanceNotFoundException;
+
+    // Autor : Brais
+    public List<Inscripcion> find (Connection connection , String emailUsuario ) ;
+
+    // Autor : Carlos
+    public  void update( Connection connection , Inscripcion inscripcion); throws InstanceNotFoundException;
+
+    // Autor : Yago
+    public void remove ( Connection connection , Inscripcion inscripcion);
+
+}
