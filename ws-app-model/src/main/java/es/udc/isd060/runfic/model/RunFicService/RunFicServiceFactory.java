29119c90c7b63ec7627c413a6b853d80e00374a2
Yago Mira <yago.mira@udc.es>
2020-11-15 14:48:38 +0100

    Model_Service Factory

--- ws-app-model/src/main/java/es/udc/isd060/runfic/model/RunFicService/RunFicServiceFactory.java
+++ ws-app-model/src/main/java/es/udc/isd060/runfic/model/RunFicService/RunFicServiceFactory.java
@@ -1,4 +1,34 @@
 package es.udc.isd060.runfic.model.RunFicService;
 
+import es.udc.ws.util.configuration.ConfigurationParametersManager;
+
 public class RunFicServiceFactory {
+
+    private final static String CLASS_NAME_PARAMETER = "RunFicServiceFactory.className";
+    private static RunFicService service = null;
+
+    private RunFicServiceFactory() {
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static RunFicService getInstance() {
+        try {
+            String serviceClassName = ConfigurationParametersManager
+                    .getParameter(CLASS_NAME_PARAMETER);
+            Class serviceClass = Class.forName(serviceClassName);
+            return (RunFicService) serviceClass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public synchronized static RunFicService getService() {
+
+        if (service == null) {
+            service = getInstance();
+        }
+        return service;
+
+    }
 }
