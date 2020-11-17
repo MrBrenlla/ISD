36552ea04e1400f88fe009675742d966d9c80afb
Yago Mira Urdampilleta <yago.mira@udc.es>
2020-11-17 15:14:05 +0100

    Revert "Revert "Merge branch 'master' of git.fic.udc.es:docencia-isd-2021/isd060""
    This reverts commit 7af193ec32f5d2f4806cc87eea9f173365446fef

--- ws-app-model/src/main/java/es/udc/isd060/runfic/model/RunFicService/RunFicServiceImpl.java
+++ ws-app-model/src/main/java/es/udc/isd060/runfic/model/RunFicService/RunFicServiceImpl.java
@@ -2,9 +2,14 @@ package es.udc.isd060.runfic.model.RunFicService;
 
 import static es.udc.isd060.runfic.model.util.ModelConstants.RUNFIC_DATA_SOURCE;
 
+import es.udc.isd060.runfic.model.RunFicService.exceptions.DorsalHaSidoRecogidoException;
+import es.udc.isd060.runfic.model.RunFicService.exceptions.NumTarjetaIncorrectoException;
 import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
 import es.udc.ws.util.sql.DataSourceLocator;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 import javax.sql.DataSource;
@@ -59,4 +64,49 @@ public class RunFicServiceImpl implements RunFicService {
     //****************************************** Carlos *************************************************
     //**************************************************************************************************
 
-}
+
+    public Carrera findCarrera ( Long idCarrera ) throws InstanceNotFoundException{
+        try (Connection connection = dataSource.getConnection()) {
+            return carreraDao.find(connection, idCarrera);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public Inscripcion recogerDorsal (Long idInscripcion,String numTarjeta) throws InstanceNotFoundException ,
+            DorsalHaSidoRecogidoException , NumTarjetaIncorrectoException {
+        try (Connection connection = dataSource.getConnection()) {
+            try {
+                connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+                connection.setAutoCommit(false);
+
+                // Buscamos la inscripcion correspondiente al id proporcionado
+                Inscripcion inscripcion = inscripcionDao.find(connection, idInscripcion);
+                // Comprobamos que el dorsal no ha sido recogido
+                if (inscripcion.isRecogido()) {
+                    throw new DorsalHaSidoRecogidoException();
+                }
+                // Comprobamos que el numTarjeta proporcionado corresponde al de la inscripcion
+                if (!inscripcion.getTarjeta().equals(numTarjeta)) {
+                    throw new NumTarjetaIncorrectoException();
+                }
+                // Todo ok
+                connection.commit();
+                return inscripcion;
+            } catch (InstanceNotFoundException e) {
+                connection.rollback();
+                throw e;
+            } catch (DorsalHaSidoRecogidoException e) {
+                connection.rollback();
+                throw e;
+            } catch (NumTarjetaIncorrectoException e) {
+                connection.rollback();
+                throw e;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
