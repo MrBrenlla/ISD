732a701d8be900e426a35005f32056c167fee583
Carlos Fernandez Arman <carlos.fernandez.arman@udc.es>
2020-12-15 22:09:53 +0100

    [Común] Corregidos errores TODO ¿ RunFicServiceService = RunFicService ?

--- ws-app-model/src/main/java/es/udc/isd060/runfic/model/RunFicServiceService/RunFicServiceService.java
+++ ws-app-model/src/main/java/es/udc/isd060/runfic/model/RunFicServiceService/RunFicServiceService.java
@@ -6,13 +6,35 @@ import es.udc.isd060.runfic.model.inscripcion.Inscripcion;
 import java.time.LocalDateTime;
 import java.util.List;
 
+// TODO PREGUNTAR A PROFESOR
+// En Realidad  CREO  QUE Es RunFicService ( Es EL problema de copiar las cosas)
+// Solo ha que hacer REFACTOR > RENAME
+// VER APUNTES TEORÍA TEMA 6
 public interface RunFicServiceService {
 
+    //**************************************************************************************************
+    //****************************************** Brais *************************************************
+    //**************************************************************************************************
+
+    Inscripcion addInscripcion ( String email , String numTarjeta , Carrera carrera );
+
+    List<Inscripcion > findInscripcion ( String email );
+
+    //**************************************************************************************************
+    //****************************************** Yago *************************************************
+    //**************************************************************************************************
+
     public Carrera addCarrera ( Carrera carrera  );
-    public Carrera findCarrera ( Integer idCarrera );
-    public Inscripcion addInscripcion ( String email , String numTarjeta , Carrera carrera );
-    public List<Inscripcion > findInscripcion ( String email );
-    public Inscripcion recogerDorsal ( Integer codReserva , String numTarjeta );
+
     public List<Carrera> findCarrera (LocalDateTime fechaCelebracion , String nombreCiudad );
+
+    //**************************************************************************************************
+    //****************************************** Carlos *************************************************
+    //**************************************************************************************************
+
+    public Carrera findCarrera ( Integer idCarrera );
+
+    public Inscripcion recogerDorsal ( Integer codReserva , String numTarjeta );
+
 
 }
