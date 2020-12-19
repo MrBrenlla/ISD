4eae3ad966107a3ba4506b0f5c58029e8fec71ab
Yago Mira <yago.mira@udc.es>
2020-12-19 19:59:47 +0100

    [Fun 1] Capa servicios - Parte 1

---
+++ ws-app-service/src/main/java/es/udc/ws/isd060/runfic/service/restservice/json/JsonToRestCarreraDtoConversor.java
@@ -0,0 +1,77 @@
+package es.udc.ws.isd060.runfic.service.restservice.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import es.udc.ws.isd060.runfic.service.restservice.dto.RestCarreraDto;
+import es.udc.ws.util.json.ObjectMapperFactory;
+import es.udc.ws.util.json.exceptions.ParsingException;
+
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class JsonToRestCarreraDtoConversor {
+
+    public static ObjectNode toObjectNode(RestCarreraDto carrera) {
+
+        ObjectNode carreraObject = JsonNodeFactory.instance.objectNode();
+
+        if (carrera.getIdCarrera() != null) {
+            carreraObject.put("idCarrera", carrera.getIdCarrera());
+        }
+        carreraObject.put("ciudadCelebracion", carrera.getCiudadCelebracion()).
+                put("descripcion", carrera.getDescripcion()).
+                put("precioInscripcion", carrera.getPrecioInscripcion()).
+                put("fechaCelebracion", carrera.getFechaCelebracion().toString()).
+                put("plazasDisponibles", carrera.getPlazasDisponibles()).
+                put("plazasOcupadas", carrera.getPlazasOcupadas());
+
+        return carreraObject;
+    }
+
+    public static ArrayNode toArrayNode(List<RestCarreraDto> carreras) {
+
+        ArrayNode carrerasNode = JsonNodeFactory.instance.arrayNode();
+        for (int i = 0; i < carreras.size(); i++) {
+            RestCarreraDto carreraDto = carreras.get(i);
+            ObjectNode carreraObject = toObjectNode(carreraDto);
+            carrerasNode.add(carreraObject);
+        }
+
+        return carrerasNode;
+    }
+
+    public static RestCarreraDto toServiceCarreraDto(InputStream jsonCarrera) throws ParsingException {
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(jsonCarrera);
+
+            if (rootNode.getNodeType() != JsonNodeType.OBJECT) {
+                throw new ParsingException("Unrecognized JSON (object expected)");
+            } else {
+                ObjectNode carreraObject = (ObjectNode) rootNode;
+
+                JsonNode carreraIdNode = carreraObject.get("idCarrera");
+                Long carreraId = (carreraIdNode != null) ? carreraIdNode.longValue() : null;
+
+                String ciudadCelebracion = carreraObject.get("ciudadCelebracion").textValue().trim();
+                String descripcion = carreraObject.get("descripcion").textValue().trim();
+                float precioInscripcion = carreraObject.get("precioInscripcion").floatValue();
+                LocalDateTime fechaCelebracion = LocalDateTime.parse(carreraObject.get("fechaCelebracion").textValue());
+                Integer plazasDisponibles = carreraObject.get("plazasDisponibles").intValue();
+                Integer plazasOcupadas = carreraObject.get("plazasOcupadas").intValue();
+
+                return new RestCarreraDto(carreraId, ciudadCelebracion, descripcion, precioInscripcion, fechaCelebracion,plazasDisponibles, plazasOcupadas);
+            }
+        } catch (ParsingException ex) {
+            throw ex;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
+    }
+
+}
--- ws-app-service/src/main/java/es/udc/ws/isd060/runfic/service/restservice/servlets/CarreraServlet.java
+++ ws-app-service/src/main/java/es/udc/ws/isd060/runfic/service/restservice/servlets/CarreraServlet.java
@@ -1,15 +1,26 @@
 package es.udc.ws.isd060.runfic.service.restservice.servlets;
 
+import es.udc.isd060.runfic.model.RunFicService.RunFicServiceFactory;
 import es.udc.isd060.runfic.model.carrera.Carrera;
+import es.udc.ws.isd060.runfic.service.restservice.dto.CarreraToRestCarreraDtoConversor;
+import es.udc.ws.isd060.runfic.service.restservice.dto.RestCarreraDto;
+import es.udc.ws.isd060.runfic.service.restservice.json.JsonToExceptionConversor;
+import es.udc.ws.isd060.runfic.service.restservice.json.JsonToRestCarreraDtoConversor;
+import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.json.exceptions.ParsingException;
+import es.udc.ws.util.servlet.ServletUtils;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class CarreraServlet {
+public class CarreraServlet extends HttpServlet {
     // TODO all
 
     // Nota LOS Métodos que se comentan por CF ( Called For) son llamados por la funcion correspondiente de la capa servicios
@@ -17,12 +28,47 @@ public class CarreraServlet {
     // VER TEMA 6 ( DIAP. 45 )
     // un doXXX puede tener más de un CF
     // Aclaracion en \docs\DiagramaServlets.dia
+
+
     //**************************************************************************************************
     //****************************************** Yago *************************************************
     //**************************************************************************************************
 
-    // CF : public Carrera addCarrera (Carrera carrera  );
+    // CF : public Carrera addCarrera (Carrera carrera);
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String path = ServletUtils.normalizePath(req.getPathInfo());
+        if (path != null && path.length() > 0) {
+            ServletUtils.writeServiceResponse(resp, HttpServletResponse.SC_BAD_REQUEST,
+                    JsonToExceptionConversor.toInputValidationException(
+                            new InputValidationException("Invalid Request: " + "invalid path " + path)),
+                    null);
+            return;
+        }
+        RestCarreraDto carreraDto;
+        try {
+            carreraDto = JsonToRestCarreraDtoConversor.toServiceCarreraDto(req.getInputStream());
+        } catch (ParsingException ex) {
+            ServletUtils.writeServiceResponse(resp, HttpServletResponse.SC_BAD_REQUEST, JsonToExceptionConversor
+                    .toInputValidationException(new InputValidationException(ex.getMessage())), null);
+            return;
+        }
+        Carrera carrera = CarreraToRestCarreraDtoConversor.toCarrera(carreraDto);
+        try {
+            carrera = RunFicServiceFactory.getService().addCarrera(carrera);
+        } catch (InputValidationException ex) {
+            ServletUtils.writeServiceResponse(resp, HttpServletResponse.SC_BAD_REQUEST,
+                    JsonToExceptionConversor.toInputValidationException(ex), null);
+            return;
+        }
+        carreraDto = CarreraToRestCarreraDtoConversor.toRestCarreraDto(carrera);
+
+        String carreraURL = ServletUtils.normalizePath(req.getRequestURL().toString()) + "/" + carrera.getIdCarrera();
+        Map<String, String> headers = new HashMap<>(1);
+        headers.put("Location", carreraURL);
+
+        ServletUtils.writeServiceResponse(resp, HttpServletResponse.SC_CREATED,
+                JsonToRestCarreraDtoConversor.toObjectNode(carreraDto), headers);
 
     }
 
